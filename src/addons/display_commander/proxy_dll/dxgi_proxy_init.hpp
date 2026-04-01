d79ae7170a36faa6c8d8d73cdc178571d5217c7e
Piotr Mikulski <piotr.mikulski@gmail.coM>
2026-04-01 16:14:51 -0700

    build(DC_NO_MODULES): skip module and proxy_dll sources; link version.lib
    Exclude modules/audio, controller, example_dummy and proxy_dll/*.cpp when
    DC_NO_MODULES is on. Inline InstallRealDXGIMinHookHooks in dxgi_proxy_init.hpp
    for that configuration. Link version.lib for GetFileVersionInfo*/VerQueryValueW
    used by main_entry after version_proxy.cpp is omitted. Update CHANGELOG v0.13.86.

--- src/addons/display_commander/proxy_dll/dxgi_proxy_init.hpp
+++ src/addons/display_commander/proxy_dll/dxgi_proxy_init.hpp
@@ -2,4 +2,9 @@
 
 // Install MinHook on real dxgi.dll CreateDXGIFactory2. Call from addon init (not DllMain).
 // No-op if real DXGI not loaded, hook suppression enabled, or already installed.
+#if defined(DC_NO_MODULES)
+// proxy_dll/*.cpp are not compiled when DC_NO_MODULES (addon-only build); dxgi_proxy.cpp would define this.
+inline void InstallRealDXGIMinHookHooks() {}
+#else
 void InstallRealDXGIMinHookHooks();
+#endif
